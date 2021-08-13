bfce665d185a430dd6958e14f1c863a60e4a46f4
chaosmaster <vvinskevitch@gmail.com>
2021-08-13 19:30:25 +0300

    first commit

---
+++ app/src/main/java/com/sharkit/busik/Sender.java
@@ -0,0 +1,14 @@
+package com.sharkit.busik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sender extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sender);
+    }
+}
